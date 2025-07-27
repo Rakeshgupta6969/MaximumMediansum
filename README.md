cecc3fd10a7d6ec0462eac21523046a6da5888f0
Rakeshgupta6969 <rg6888407@gmaol.com>
2025-07-27 22:59:03 +0530

    optimal solution

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # MaximumMediansum
+using the optimal solution.
---
+++ sol.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long maximumMedianSum(vector<int>& nums) {
+
+        sort(nums.begin(),nums.end());
+
+        long long ans = 0;
+        int n = nums.size();
+        for(int i = n/3; i<n; i = i+2){
+            ans += nums[i];
+        }
+
+     return ans;
+        
+    }
+};
